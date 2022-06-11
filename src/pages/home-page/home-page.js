1f2f5187ab104c5ebe8460f5e716f6b2ab0d1261
evlev777 <levkovich.evgeniy.98@gmail.com>
2022-06-11 17:03:09 +0300

    add react-router

---
+++ src/pages/home-page/home-page.js
@@ -0,0 +1,9 @@
+import React from "react"
+import './home-page.css';
+export const  HomePage = () => {
+    return(
+        <div className="home-page">
+            HomePage
+        </div>
+    )
+}
